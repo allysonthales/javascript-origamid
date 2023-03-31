75f22748312b9e29fe6f269522508f3bcffe1edc
Allyson Thales <allysonthales@protonmail.com>
2023-03-31 10:09:51 -0300

    correcao exercicio 01 - variaveis

--- 01-exercicio-variaveis.js
+++ 01-exercicio-variaveis.js
@@ -1,13 +1,13 @@
 // Declarar uma variável com o seu nome
-
+var nome = "Allyson";
 // Declarar uma variável com a sua idade
 var idade = 28;
 
 // Declarar uma variável com a sua comida
 // favorita e não atribuir valor
-var comida;
+var comidaFavorita;
 
 // Atribuir valor a sua comida favorita
-comida = "Pizza";
+comidaFavorita = "Pizza";
 // Declarar 5 variáveis diferentes sem valores
 var hora, dia, semana, mes, anos;
