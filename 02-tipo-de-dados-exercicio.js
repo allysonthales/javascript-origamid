d42f4449fafd11cac0100a423ed3b3badcfa78bf
Allyson Thales <allysonthales@protonmail.com>
2023-03-31 11:00:15 -0300

    tipo de dados exercicio 02

---
+++ 02-tipo-de-dados-exercicio.js
@@ -0,0 +1,15 @@
+// Declare uma variável contendo uma string
+var string = "Hello, world!";
+// Declare uma variável contendo um número dentro de uma string
+var stringNumero = "variavel 2";
+// Declare uma variável com a sua idade
+var idade = "28";
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = "Allyson";
+var sobrenome = "Thales";
+var nomeCompleto = nome + " " + sobrenome;
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
