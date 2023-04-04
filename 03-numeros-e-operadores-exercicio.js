2a4c721f7852d878cdd598351f76f639043a065a
Allyson Thales <allysonthales@protonmail.com>
2023-04-03 21:38:36 -0300

    exericico 03 - numeros e operadores

---
+++ 03-numeros-e-operadores-exercicio.js
@@ -0,0 +1,18 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + (5 * 2) / 2 + 20;
+// Resposta = 35
+
+// Crie duas expressões que retornem NaN
+var expressao1 = "primeiro" - 10;
+var expressao2 = "segundo" / 10;
+
+// Somar a string '200' com o número 50 e retornar 250
+var soma = +"200" + 50;
+// Incremente o número 5 e retorne o seu valor incrementado
+var x = 5;
+x++;
+console.log(x);
+// Como dividir o peso por 2?
+var numero = "80";
+var unidade = "kg";
+var peso = +numero / 2 + unidade; // '80kg'
