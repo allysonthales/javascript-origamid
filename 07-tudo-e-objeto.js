d9c2c2087c8576351e2543c3ab50670909663722
Allyson Thales <allysonthales@protonmail.com>
2023-04-05 21:13:01 -0300

    exercicios 06 e 07

---
+++ 07-tudo-e-objeto.js
@@ -0,0 +1,9 @@
+// nomeie 3 propriedades ou métodos de strings
+// resposta = lenght, toUpperCase() e trim()
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+// resposta = location, firstChild(), append(), querySelectorAll() e anchors
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+// resposta = navigator.clipboard.writeText(texto), dentro de um event listener;
