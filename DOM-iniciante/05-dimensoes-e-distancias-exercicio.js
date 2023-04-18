b5240e73013d04abbda0990231c5928558a92cdf
Allyson Thales <allysonthales@protonmail.com>
2023-04-18 10:44:06 -0300

    exercicio 05 - dimensoes e distancias

--- DOM-iniciante/05-dimensoes-e-distancias-exercicio.js
+++ DOM-iniciante/05-dimensoes-e-distancias-exercicio.js
@@ -4,29 +4,44 @@ const primeiraImagem = document.querySelector("img");
 console.log(primeiraImagem.offsetTop);
 
 // Retorne a soma da largura de todas as imagens
-let soma =0;
-const imagens = document.querySelectorAll("img")
-imagens.forEach(item => {
-  soma += item.clientWidth;
-})
-console.log(soma);
+
+const imagens = document.querySelectorAll("img");
+
+function somaImagens() {
+  let soma = 0;
+  imagens.forEach((item) => {
+    soma += item.offsetWidth;
+  });
+  console.log(soma);
+}
+
+window.onload = function () {
+  somaImagens();
+};
+
 // Verifique se os links da página possuem
 // o mínimo recomendado para telas utilizadas
 // com o dedo. (48px/48px de acordo com o google)
-const links = document.querySelectorAll('.menu a')
+const links = document.querySelectorAll(".menu a");
 
-// menu.forEach
+links.forEach((item) => {
+  const linkWidth = item.clientWidth;
+  const linkHeight = item.clientHeight;
+  if (linkWidth >= 48 && linkHeight >= 48) {
+    console.log("Possui acessibilidade");
+  } else {
+    console.log("Não possui acessibilidade");
+  }
+
+  // console.log(`${item.offsetWidth} offsetwidth`);
+  // console.log(`${item.clientWidth} clientWidth`);
+});
 
 // Se o browser for menor que 720px,
 // adicione a classe menu-mobile ao menu
 
-const menu = document.querySelector('.menu')
-const tela = window.matchMedia("(max-width: 720px)")
-if(tela.matches){
-  
-}else{
-  console.log('false');
+const menu = document.querySelector(".menu");
+const tela = window.matchMedia("(width < 720px)");
+if (tela.matches) {
+  menu.classList.add("menu-mobile");
 }
-
-const telaAtual = window.innerWidth
-console.log(telaAtual);
