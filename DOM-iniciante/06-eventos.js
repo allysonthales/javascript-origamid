e6173fe1970068b499ad0751b24bef75ab4ad345
Allyson Thales <allysonthales@protonmail.com>
2023-04-18 11:37:39 -0300

    exericicio 06-eventos

--- DOM-iniciante/06-eventos.js
+++ DOM-iniciante/06-eventos.js
@@ -2,11 +2,51 @@
 // adicione a classe ativo ao item clicado e remova dos
 // demais itens caso eles possuam a mesma. Previna
 // o comportamento padrão desses links
+const linksInternos = document.querySelectorAll('a[href^="#');
+linksInternos.forEach((item) => {
+  item.addEventListener("click", handleClick);
+});
+
+function handleClick(event) {
+  event.preventDefault();
+
+  linksInternos.forEach((item) => {
+    item.classList.remove("ativo");
+  });
+
+  event.target.classList.add("ativo");
+}
 
 // Selecione todos os elementos do site começando a partir do body,
 // ao clique mostre exatamente quais elementos estão sendo clicados
 
+const todosElementos = document.querySelectorAll("body *");
+todosElementos.forEach((item) => {
+  item.addEventListener("click", handleItem);
+});
+function handleItem(event) {
+  console.log(event.target);
+  event.stopPropagation();
+}
+
 // Utilizando o código anterior, ao invés de mostrar no console,
 // remova o elemento que está sendo clicado, o método remove() remove um elemento
 
+todosElementos.forEach((item) => {
+  item.addEventListener("click", removeClick);
+});
+
+function removeClick(event) {
+  event.target.remove();
+}
+
 // Se o usuário clicar na tecla (t), aumente todo o texto do site.
+
+document.addEventListener("keydown", keyDown);
+tamanho = 16;
+function keyDown(event) {
+  if (event.key == "t") {
+    tamanho++;
+    document.body.style.fontSize = `${tamanho}px`;
+  }
+}
