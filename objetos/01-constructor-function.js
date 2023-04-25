7f6bf2f69cce3dbefb3d18506e223c27e080ffb4
Allyson Thales <allysonthales@protonmail.com>
2023-04-25 10:56:27 -0300

    exercicio 01 - objetos

--- objetos/01-constructor-function.js
+++ objetos/01-constructor-function.js
@@ -7,9 +7,9 @@
 //   },
 // };
 
-function Pessoa() {
-  this.nome = "Nome pessoa";
-  const idade = 0;
+function Pessoa(nome, idade) {
+  this.nome = nome;
+  this.idade = idade;
   this.andar = function () {
     console.log(this.nome + " andou");
   };
@@ -18,6 +18,10 @@ function Pessoa() {
 // Crie 3 pessoas, João - 20 anos,
 // Maria - 25 anos, Bruno - 15 anos
 
+const joao = new Pessoa("João", 20);
+const maria = new Pessoa("Maria", 25);
+const bruno = new Pessoa("Bruno", 15);
+
 // Crie uma Constructor Function (Dom) para manipulação
 // de listas de elementos do dom. Deve conter as seguintes
 // propriedades e métodos:
@@ -25,3 +29,20 @@ function Pessoa() {
 // elements, retorna NodeList com os elementos selecionados
 // addClass(classe), adiciona a classe a todos os elementos
 // removeClass(classe), remove a classe a todos os elementos
+
+function Dom(seletor) {
+  const nodeList = document.querySelectorAll(seletor);
+  this.elements = function () {
+    return nodeList;
+  };
+  this.addClass = function (classe) {
+    nodeList.forEach((item) => {
+      item.eList.add(classe);
+    });
+  };
+  this.removeClass = function (classe) {
+    nodeList.forEach((item) => {
+      item.classList.remove(classe);
+    });
+  };
+}
