551df1c6203db18eceb46aa5a0f4a8ecbe44b332
Allyson Thales <allysonthales@protonmail.com>
2023-05-01 06:29:57 -0300

    exercicio 03 - host

---
+++ objetos/03-native-host-user.js
@@ -0,0 +1,16 @@
+// Liste 5 objetos nativos
+Object;
+Number;
+String;
+Math;
+Function;
+
+// Liste 5 objetos do browser
+Alert;
+history;
+document;
+HTMLCollection;
+nodeList;
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
