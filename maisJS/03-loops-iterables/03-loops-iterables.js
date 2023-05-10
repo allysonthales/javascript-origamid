2fa856a23140e77d338c249869038e81e2460773
Allyson Thales <allysonthales@protonmail.com>
2023-05-10 11:11:27 -0300

    atividade loops e iterables

--- maisJS/03-loops-iterables/03-loops-iterables.js
+++ maisJS/03-loops-iterables/03-loops-iterables.js
@@ -1,7 +1,16 @@
 // Crie 4 li's na página
 // Utilizando o for...of
 // adicione uma classe a cada li
+const li = document.querySelectorAll("li");
+
+for (item of li) {
+  item.classList.add("teste");
+}
 
 // Utilize o for...in para listar
 // todos as propriedades e valores
 // do objeto window
+
+for (style in window) {
+  console.log(`${style} ${window[style]}`);
+}
